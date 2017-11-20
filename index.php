4c074ff32ac72da590e0e97ee97925022f54574d
dupres <stephen.dupre@hotmail.fr>
2017-11-20 15:32:07 +0100

    Added chromarrow background

--- index.php
+++ index.php
@@ -9,12 +9,15 @@
 
 <body>
 
+	<canvas id="bg"></canvas>
 	<div id="content">
-			<div id="card">
-				<div id="card_content">
-					<img id="card_front" src="./sp/sp_forward.jpg"/>
-					<canvas id="card_back"/>
-				</div>
+		<div id="card">
+			<div id="card_content"  onmousedown="event.preventDefault ? event.preventDefault() : event.returnValue = false">
+				<div id="card_hover">Click me !</div>
+				<img id="card_front" src="./sp/sp_forward.jpg"/>
+				<canvas id="card_back"/>
+			</div>
+
 		</div>
 	</div>
 
@@ -35,20 +38,51 @@ body{
 	align-items:center;
 }
 
+#bg{
+	background-color: black;
+	position:fixed;
+	height:100%;
+	width:100%;
+	top:0;
+	left:0;
+	z-index:-1000;
+}
+
+#content{
+	z-index: 0;
+	min-height:100%;
+	width:80vh;
+	background-color: rgba(255,255,255,0.85);
+	border-radius:30px;
+}
+
 #card{
-	width:90vh;
-	height:90vh;
+	width:40vh;
+	height:40vh;
 	display:flex;
 	flex-direction:column;
 	align-items:center;
 	position:relative;
-
+}
+@font-face {
+    font-family: typoRound;
+    src: url(Typo_Round.otf);
+}
+#card_hover{
+	font-family: typoRound;
+	z-index:42;
+	position:relative;
+	font-size: 2vh;
+	visibility: visible;
 }
 #card_content{
+	border:3px outline black;
 	position:relative;
 	width:100%;
 	height:100%;
 	transition: 0.5s ease-in-out all;
+	box-shadow: 5px 10px 10px #777;
+	transform:rotate(-3deg);
 }
 #card_front{
 	position:absolute;
@@ -65,8 +99,6 @@ body{
 	top:0px;
 	left:0px;
 	z-index:1;
-	
-	
 }
 
 
@@ -79,23 +111,136 @@ body{
 </style>
 <script>
 
+	class Shot{
+    constructor(posX, posY=0, dir="bottom", type=1, r=255, g=255, b=255, speed=1, dmg=1){
+        this.type = type;
+        this.posX = posX;
+        this.posY = posY;
+        this.dir = dir;
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.speed = speed;
+        this.dmg = dmg;
+        this.hit = false;
+        
+    }
+    
+    act(canvas, context){
+        if (this.dir == "bottom"){
+            if (this.posY + this.speed < canvas.height){
+                this.posY=this.posY+this.speed;
+                this.draw(canvas,context);
+            }else{
+                this.hit = true;
+            }
+        }else if (this.dir == "top"){
+            if (this.posY - this.speed > 0){
+                this.posY=this.posY-this.speed;
+                this.draw(canvas,context);
+            }else{
+                this.hit = true;
+            }
+        }
+    }
+    
+    draw(canvas,context){
+        context.beginPath();
+        context.strokeStyle = "rgb("+this.r+","+this.g+","+this.b+")";
+        if (this.dir == "top"){
+            context.moveTo(this.posX, this.posY);
+            context.lineTo(this.posX, this.posY + 5 * this.speed);
+        }else if(this.dir == "bottom"){
+            context.moveTo(this.posX, this.posY);
+            context.lineTo(this.posX, this.posY - 5 * this.speed);
+        }
+        context.closePath();
+        context.stroke();
+    }
+    
+}
+
+
+	window.onload = function()
+	{
+	    //---------------------------------------------------------
+	    //                        Initialisation
+	    //---------------------------------------------------------
+	    
+	    var canvas = document.getElementById('bg');
+	    if(!canvas)
+	    {
+	        alert("Impossible de récupérer le canvas");
+	        return;
+	    }
+
+	    var context = canvas.getContext('2d');
+	    if(!context)
+	    {
+	        alert("Impossible de récupérer le context du canvas");
+	        return;
+	    }
+	    
+	    var shots = [];
+	    var colors = [[255,0,0],[255,255,0],[255,0,255],[0,255,255],[0,255,0],[0,0,255]];
+	    
+	    //posX, posY=0, dir="bottom", type=1, r=255, g=255, b=255, speed=1, dmg=1
+	    
+	    
+	    //drawArrow(context,0,0,0.5,'rgb(255,0,0)','rgb(0,0,255)');
+	    
+	    
+	    // Background variables
+	    var interval = 0;
+	    var intervalPourRendreLesGensFous = 0;
+	    
+	    //---------------------------------------------------------------------
+	    //                  Press space to play screen
+	    //---------------------------------------------------------------------
+
+	    interval = setInterval(function(){
+	        context.beginPath();
+	        context.fillStyle="rgba(0,0,0,0.3)";
+	        context.rect(0,0,canvas.width,canvas.height);
+	        context.fill();
+	        context.closePath();
+	        
+	        $.each(shots, function(key,shot){
+	            shot.act(canvas,context);
+	            if (shot.hit === true){
+	                posX = Math.floor((Math.random() * canvas.width) + 1);
+	                color = colors[Math.floor((Math.random() * colors.length))];
+	                speed = (Math.random() * 2) + 1;
+	                shots[key] = new Shot(posX,0,"bottom",1,color[0],color[1],color[2],speed);
+	            }
+	        });
+	        if (Math.floor((Math.random() * 50) + 1) == 1 && shots.length<100){
+	            posX = Math.floor((Math.random() * canvas.width) + 1);
+	            color = colors[Math.floor((Math.random() * colors.length))];
+	            speed = (Math.random() * 2) + 1;
+	            shots[shots.length] = new Shot(posX,0,"bottom",1,color[0],color[1],color[2],speed);
+	        }
+	        
+	    },20);
+	}
+
+
+
 	//SP AVATAR
 	//http://www.sp-studio.de/
 
-
-
 	var card_canvas = $("#card_back");
-	console.log(card_canvas);
-	var context = card_canvas.get(0).getContext("2d");
-	context.moveTo(0,0);
-	context.fillStyle="blue";
-	context.fillRect(0,0,card_canvas.width(),card_canvas.height());
+	var card_context = card_canvas.get(0).getContext("2d");
+	card_context.moveTo(0,0);
+	card_context.fillStyle="lightgrey";
+	card_context.fillRect(0,0,card_canvas.width(),card_canvas.height());
 
 	//FLIP 
 	//FROM https://davidwalsh.name/css-flip
-	card_face = "front";
+	var card_face = "front";
 	function card_flip(){
 		if (card_face == "front"){
+			card_hover_firsttime = false;
 			card_face = "back";
 			$("#card_content").css("width","0%");
 			setTimeout(function(){
@@ -108,10 +253,13 @@ body{
 			setTimeout(function(){
 				$("#card_back").css("z-index","1");
 				$("#card_content").css("width","100%");
+				setTimeout(function(){card_hover_display = true;},500);
 			},500);
 		}
 	}
 	$("#card").click(function(){
+		card_hover_display = false;
+		$("#card_hover").css("visibility","hidden");
 		card_flip();
 	});
 	
@@ -159,10 +307,36 @@ body{
     		nextBlink = cpt + Math.floor((Math.random() * 75) + 1)+25;
     		stay = 3;
     	}
-
     	cpt=cpt+1;
-    },50);
+    },40);
 
-
+    // -----   Card_hover -------
+    
+	var card_hover_display = true;
+	var card_hover_state = "small";
+	var card_hover_firsttime = true;
+	setInterval(function(){
+		if (card_hover_state == "small"){
+			card_hover_state = "big";
+			$("#card_hover").animate({fontSize: "4vh"});
+		}else{
+			card_hover_state = "small";
+			$("#card_hover").animate({fontSize: "3vh"});
+		}
+	},400);
+    $("#card").on("mouseover",function(){
+    	if (!(card_hover_firsttime)){
+	    	if (card_hover_display){
+		    	if (card_face == "front"){
+		    		$("#card_hover").css("visibility","visible");
+		    	}
+		    }
+		}
+    });
+    $("#card").on("mouseout",function(){
+    	if (!(card_hover_firsttime)){
+    		$("#card_hover").css("visibility","hidden");
+    	}
+    });
 
 </script>
